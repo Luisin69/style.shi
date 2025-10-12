1dfa78aa67bbc9842fc73792963e1c5c127878c1
AndAyalaL <misterstudy69@gmail.com>
2025-10-12 13:15:34 -0700

    migraciones para la tabla de productos

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -11,7 +11,14 @@
 
 
     <div class="card">
-
+        <img src="https://media.licdn.com/dms/image/v2/D5603AQEWbu2hOV6hGA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1706800521454?e=2147483647&v=beta&t=Il6DBgOCmJx8Bn9ZHcf1h7TXJ_UsjDDvL7JIn6Lh3xI" alt="DIO">
+        <div class="card-details">
+            <span>Muneco inflable sexual de luisin</span>
+            <div class="card-action">
+                <span>500$</span>
+                <button class="buy-button">+</button>
+            </div>
+        </div>
     </div>
 
 
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProductsController;
 
 Route::get('/', function () {
     return view('login');
@@ -10,9 +11,9 @@ Route::get('/', function () {
 Route::view('/login', "login")->name('login');
 Route::view('/registro', "register")->name('registro');
 Route::view('/privada', "secret")->name('privada');
-Route::view('/productos',"products")->name('productos');
+//Route::view('/productos',"products")->name('productos');
 
 Route::post('/validar-registro', [LoginController::class,'register'])->name('validar-registro');
 Route::post('/inicia-sesion', [LoginController::class,'login'])->name('inicia-sesion');
 Route::get('/logout', [LoginController::class,'logout'])->name('logout');
-
+Route::get('/productos',[ProductsController::class,'index'])->name('productos');
