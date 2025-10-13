46a64cf62343b6bb88fd2034f2172b0cfb3cbf86
luisin <luisvl26@hotmail.com>
2025-10-13 08:36:33 -0700

    cambie una ruta noma

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -6,7 +6,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Style.shi</title>
         <link rel="stylesheet" href="{{ asset('css/cards.css') }}">
-        <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
     </head>
 
     <body>
