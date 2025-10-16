368db92980bfd8167e36b9e35d9e3886746a5c5b
AndAyalaL <misterstudy69@gmail.com>
2025-10-15 20:19:20 -0700

    oal

--- database/migrations/2025_10_12_193140_create-products-migration.php
+++ database/migrations/2025_10_12_193140_create-products-migration.php
@@ -15,7 +15,6 @@ return new class extends Migration
         Schema::create('products',function(Blueprint $table){
             $table->id();
             $table->string('name',255);
-            $table->string('image',500);
             $table->integer('price');
         });
     }
